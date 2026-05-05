b2dc198219ef7f9414e857a242bd5e35a69d8c4b
mjdiazdev <mjdiaz.dev@gmail.com>
2026-05-05 14:28:52 -0400

    inicio Proyecto

---
+++ educativa_APP/educativa_APP/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 5/5/2026
+ * Time: 2:26 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace educativa_APP
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
